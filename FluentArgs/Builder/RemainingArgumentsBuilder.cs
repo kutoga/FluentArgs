9e6e5eb75e357bb79dc1a3be223ada39a9f5efbc
kutoga <benjmeier@bluewin.ch>
2019-07-11 21:37:20 +0200

    Include remaining arguments (#10)

---
+++ FluentArgs/Builder/RemainingArgumentsBuilder.cs
@@ -0,0 +1,39 @@
+﻿namespace FluentArgs.Builder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class RemainingArgumentsBuilder<TFunc, TFuncAsync, TParam> : IConfigurableRemainingArguments<TFunc, TFuncAsync, TParam>
+    {
+        public IParsable Call(TFunc callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IParsable Call(TFuncAsync callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfigurableRemainingArguments<TFunc, TFuncAsync, TParam> WithDescription(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfigurableRemainingArguments<TFunc, TFuncAsync, TParam> WithExamples(TParam example, params TParam[] moreExamples)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfigurableRemainingArguments<TFunc, TFuncAsync, TParam> WithExamples(string example, params string[] moreExamples)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfigurableRemainingArguments<TFunc, TFuncAsync, TParam> WithParser(Func<string, TParam> parser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FluentArgs/Description/RemainingArguments.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentArgs.Description
+{
+    using System;
+
+    internal class RemainingArguments
+    {
+        public RemainingArguments(string description, Type type, Func<string, object> parser)
+        {
+            Description = description;
+            Type = type;
+            Parser = parser;
+        }
+
+        public string? Description { get; set; }
+
+        public Type Type { get; }
+
+        public Func<string, object>? Parser { get; set; }
+    }
+}
