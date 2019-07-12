873761409f0d2e0977d46972b5ebaed83aa4abf0
Benjamin B. Meier <benjamin.meier70@gmail.com>
2019-07-12 13:19:52 +0200

    Fixing argument parsing

---
+++ FluentArgs/ArgumentExtraction/ArgumentExtractor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Immutable;
+
+namespace FluentArgs.ArgumentExtraction
+{
+    public class ArgumentExtractor
+    {
+        private IImmutableList<IImmutableList<string>> argumentGroups;
+
+        public ArgumentExtractor(string[] arguments)
+        {
+            argumentGroups = ImmutableList<IImmutableList<string>>.Empty.Add(arguments.ToImmutableList());
+        }
+
+        public bool Extract(string firstArgument, int followingArgumentsToInclude = 0)
+        {
+            foreach (var list in argumentGroups)
+        }
+    }
+}
