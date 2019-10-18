6c5a737d3d77600589bf80525133a8ea512f7096
Benjamin B. Meier <benjamin.meier70@gmail.com>
2019-10-18 05:39:41 +0200

    Add a README_nuget markdown file

--- build_readme.sh
+++ build_readme.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
+
 echo "$(date)   Start building README.md..."
 python3 ./doc/scripts/include_resolve.py ./doc/README.md.template > ./README.md
 echo "$(date)   Built README.md..."
+
+echo "$(date)   Start building README_nuget.md..."
+python3 ./doc/scripts/include_resolve.py ./doc/README_nuget.md.template > ./doc/README_nuget.md
+echo "$(date)   Built README_nuget.md..."
 
